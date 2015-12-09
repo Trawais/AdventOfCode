14af18482e592765dfc722d986f523ee383ea56d
petr.schreiber <petr.schreiber@avg.com>
2015-12-09 12:06:48 +0100

    Better iterating over characters
    each_char, referencing the file in safer way

--- 1/solve.rb
+++ 1/solve.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#x!/usr/bin/env ruby
 
 input_file = File.join(File.dirname(__FILE__), 'input.txt')
 content = File.read(input_file)
@@ -7,13 +7,13 @@ floor_counter = 0
 position = 1
 first_time = true
 
-content.split("").each do |e|
-  if e == '('
+content.each_char do |c|
+  if c == '('
     floor_counter += 1
-  elsif e == ')'
+  elsif c == ')'
     floor_counter -= 1
   else
-    puts "invalid symbol: #{e}"
+    puts "invalid symbol: #{c}"
   end
 
   if floor_counter == -1 && first_time
